4c7f6d58fe6f984b6d6c748bf993ca5154324d05
rlane85 <44262357+rlane85@users.noreply.github.com>
2019-05-30 21:17:54 -0400

    2 messages for morning: forecast and launch

--- daily.js
+++ daily.js
@@ -14,46 +14,40 @@ const service = twilio.notify.services(process.env.TWILIO_NOTIFY_SERVICE_SID);
 const bindings = numbers.map(number => {
   return JSON.stringify({ binding_type: 'sms', address: number });
 });
-  console.log(numbers)
-  request(config.DS_OPTIONS, (err, response, dsData) => {
-    if (err) { return console.log(err); }
-    notification = service.notifications
-      .create({
-        toBinding: bindings,
-        body: (`
-Forecast for ${dateFormat(dsData.daily.data[1].time * 1000, "ddd m/d")}:
+console.log(numbers)
+request(config.DS_OPTIONS, (err, response, dsData) => {
+  if (err) { return console.log(err); }
+  notification = service.notifications
+    .create({
+      toBinding: bindings,
+      body: (`
+Forecast for ${dateFormat(dsData.daily.data[0].time * 1000, "ddd m/d")}:
 ${dsData.daily.data[1].summary}
-Sunrise: ${dateFormat(dsData.daily.data[1].sunriseTime * 1000, "h:MM")}
-Sunset: ${dateFormat(dsData.daily.data[1].sunsetTime * 1000, "h:MM")}
-Moonphase: ${dsData.daily.data[1].moonPhase * 100}%
-Feels Like: ${dsData.daily.data[1].apparentTemperatureLow}° - ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
-Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
+Sunrise: ${dateFormat(dsData.daily.data[0].sunriseTime * 1000, "h:MM")}
+Sunset: ${dateFormat(dsData.daily.data[0].sunsetTime * 1000, "h:MM")}
+Moonphase: ${dsData.daily.data[0].moonPhase * 100}%
+Feels Like: ${dsData.daily.data[0].apparentTemperatureLow}° - ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
+Rain Chance: ${dsData.daily.data[0].precipProbability * 100}%
 Powered by darksky.net`)
-      })
-  });
-
-  request(config.WU_OPTIONS, (err, response, data) => {
-    if (err) { return console.log(err); }
-    notification = service.notifications
-      .create({
-        toBinding: bindings,
-        body: (`
-Currently:
-Temp: ${data.observations[0].imperial.temp}°
-Wind Speed: ${data.observations[0].imperial.windSpeed} mph
-Wind Direction: ${data.observations[0].winddir}
-Rain Today: ${data.observations[0].imperial.precipTotal} inches`)
-      })
-  });
-  request(config.WUSUMMARY_OPTIONS, (err, response, data) => {
-    if (err) { return console.log(err); }
-    notification = service.notifications
-      .create({
-        toBinding: bindings,
-        body: (`
-Today's summary from WU Station ${data.summaries[6].stationID}
-Heat index high: ${data.summaries[6].imperial.heatindexHigh}
-Wind gust high: ${data.summaries[6].imperial.windgustHigh} mph 
+    })
+});
+request(config.LAUNCH_OPTIONS, (err, response, data) => {
+  if (err) { return console.log(err); }
+  if (data.results[0].status.id == '3') {
+    var success = 1
+  }
+  else {
+    var success = 0
+  }
+  notification = service.notifications
+    .create({
+      toBinding: bindings,
+      body: (`
+Next launch at Cape Canaveral, FL: ${dateFormat(data.results[success].net, "ddd m/d 'at' h:MM t")}
+Rocket: ${data.results[success].rocket.configuration.name}
+Launch Agency: ${data.results[success].rocket.configuration.launch_service_provider}
+Mission: ${data.results[success].mission.name}
+Status: ${data.results[success].status.name}
 `)
-      })
-  })
+    });
+});
