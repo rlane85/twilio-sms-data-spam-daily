ae56d1a23a6cc0575899b25c316332f2bb91c00c
rlane85 <44262357+rlane85@users.noreply.github.com>
2019-06-11 05:53:02 -0400

    test

--- test.js
+++ test.js
@@ -10,10 +10,10 @@ numbers = ['+13214033188', '+13213683060', '+13219600909', '+13212056878'];
 const bindings = numbers.map(number => {
   return JSON.stringify({ binding_type: 'sms', address: number });
 });
-function dsForecast(body) {
+function dsForecast() {
 request(config.DS_OPTIONS, (err, response, dsData) => {
   if (err) { return console.log(err); }
-      return body = `
+      return  `
 Forecast for ${dateFormat(dsData.daily.data[1].time * 1000, "ddd m/d")}:
 ${dsData.daily.data[1].summary}
 Sunrise: ${dateFormat(dsData.daily.data[1].sunriseTime * 1000, "h:MM")}
@@ -24,7 +24,7 @@ Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
 Powered by darksky.net`
 });
 };
-function launch(body) {
+function launch() {
 request(config.LAUNCH_OPTIONS, (err, response, data) => {
   if (err) { return console.log(err); }
   if (data.results[0].status.id == '3') {
@@ -33,7 +33,7 @@ request(config.LAUNCH_OPTIONS, (err, response, data) => {
   else {
     var success = 0
   }
-    return body = body + `
+    return `
 Next launch at Cape Canaveral, FL: ${dateFormat(data.results[success].net, "ddd m/d 'at' h:MM t")}
 Rocket: ${data.results[success].rocket.configuration.name}
 Launch Agency: ${data.results[success].rocket.configuration.launch_service_provider}
@@ -42,6 +42,4 @@ Status: ${data.results[success].status.name}
 `
 });
 };
-dsForecast();
-launch();
-console.log(body);
+console.log(dsForecast()+launch());
