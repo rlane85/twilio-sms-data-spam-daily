b7cd6059555a7db82fd7cc6c34b776b0c1494507
Ryan Lane <ryanlane1985@gmail.com>
2019-06-13 14:27:08 -0400

    formatting

--- evening.js
+++ evening.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require('request-promise');
 const config = require('./config');
 const twilio = require('twilio')(
   process.env.TWILIO_ACCOUNT, process.env.TWILIO_TOKEN
@@ -24,7 +24,9 @@ Sunset: ${dateFormat(dsData.daily.data[1].sunsetTime * 1000, "h:MM")}
 Moonphase: ${dsData.daily.data[1].moonPhase * 100}%
 Feels Like: Low ${dsData.daily.data[1].apparentTemperatureLow}° - High ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
 Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
-Powered by darksky.net`});
+Powered by darksky.net
+
+`});
 request(config.LAUNCH_OPTIONS, (err, response, data) => {})
 .then(function(data) {
   launchMsg = `
@@ -32,7 +34,9 @@ Next launch at Cape Canaveral, FL: ${dateFormat(data.results[0].net, "ddd m/d 'a
 Rocket: ${data.results[0].rocket.configuration.name}
 Launch Agency: ${data.results[0].rocket.configuration.launch_service_provider}
 Mission: ${data.results[0].mission.name}
-Status: ${data.results[0].status.name}`})
+Status: ${data.results[0].status.name}
+
+`})
 .then(function() {
   epiMsg = `
 For more info please reply to this number with a keyword: current, summary, launch, or forecast (defaults to tomorrow; add keyword "today" to see the today's forecast - eg "forecast today").
