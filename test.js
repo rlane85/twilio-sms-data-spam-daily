5faaf65a7d68c4ed04c983bd9960105d3e4c2020
Ryan Lane <ryanlane1985@gmail.com>
2019-06-13 13:54:50 -0400

    test evening with promise chain

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-// Grab the two libraries we use for getting data and sending messages
-const request = require('request-promise');
+const request = require('request');
 const config = require('./config');
+const twilio = require('twilio')(
+  process.env.TWILIO_ACCOUNT, process.env.TWILIO_TOKEN
+);
 var dateFormat = require('dateformat');
-var msg;
-var numbers = new Array();
-numbers = ['+13214033188', '+13213683060', '+13219600909', '+13212056878'];
 
+var numbers = new Array();
+numbers = ['+13214033188'];
+
+const service = twilio.notify.services(process.env.TWILIO_NOTIFY_SERVICE_SID);
 
 const bindings = numbers.map(number => {
   return JSON.stringify({ binding_type: 'sms', address: number });
@@ -13,26 +16,35 @@ const bindings = numbers.map(number => {
 
 request(config.DS_OPTIONS, (err, response, dsData) => {})
 .then(function(dsData) {
-  //.catch((err) => { return console.log(err) })
   dsMsg = `
 Forecast for ${dateFormat(dsData.daily.data[1].time * 1000, "ddd m/d")}:
 ${dsData.daily.data[1].summary}
 Sunrise: ${dateFormat(dsData.daily.data[1].sunriseTime * 1000, "h:MM")}
 Sunset: ${dateFormat(dsData.daily.data[1].sunsetTime * 1000, "h:MM")}
 Moonphase: ${dsData.daily.data[1].moonPhase * 100}%
-Feels Like: ${dsData.daily.data[1].apparentTemperatureLow}° - ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
+Feels Like: Low ${dsData.daily.data[1].apparentTemperatureLow}° - High ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
 Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
-Powered by darksky.net`
-})
+Powered by darksky.net`});
 request(config.LAUNCH_OPTIONS, (err, response, data) => {})
 .then(function(data) {
-  msg = `
+  launchMsg = `
 Next launch at Cape Canaveral, FL: ${dateFormat(data.results[0].net, "ddd m/d 'at' h:MM t")}
 Rocket: ${data.results[0].rocket.configuration.name}
 Launch Agency: ${data.results[0].rocket.configuration.launch_service_provider}
 Mission: ${data.results[0].mission.name}
-Status: ${data.results[0].status.name}
-`})
+Status: ${data.results[0].status.name}`})
 .then(function() {
-  console.log(msg+dsMsg);
+  epiMsg = `
+For more info please reply to this number with a keyword: current, summary, launch, or forecast (defaults to tomorrow; add keyword "today" to see the today's forecast - eg "forecast today").
+Link to known issues and list of things I would like to add: https://ahomeconnected.com/index.php/workin-on-it/
+Github: 
+https://github.com/rlane85/twilio-sms-data-spam-daily
+https://github.com/rlane85/twilio-sms-data-spam`})
+.then(function() {
+  console.log(launchMsg+dsMsg+epiMsg);
+  notification = service.notifications
+  .create({
+    toBinding: bindings,
+    body: launchMsg+dsMsg+epiMsg
+  });
 });
