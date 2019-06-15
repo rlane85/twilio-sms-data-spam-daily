9301f73c1b3c9dccec0641068462f1012f3ceddb
rlane85 <ryanlane1985@gmail.com>
2019-06-14 20:53:31 -0400

    commit

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ Moonphase: ${dsData.daily.data[1].moonPhase * 100}%
 Feels Like: Low ${dsData.daily.data[1].apparentTemperatureLow}° - High ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
 Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
 Powered by darksky.net
-`});
+`});*/
 request(config.LAUNCH_OPTIONS, (err, response, data) => {})
 .then(function(data) {
     launchMsg = `
@@ -34,17 +34,17 @@ request(config.LAUNCH_OPTIONS, (err, response, data) => {})
     Launch Agency: ${data.results[0].rocket.configuration.launch_service_provider}
     Mission: ${data.results[0].mission.name}
     Status: ${data.results[0].status.name}
-    `})*/
-//.then(function() {
+    `})
+.then(function() {
     epiMsg = `
     For more info please reply to this number with a keyword: current, summary, launch, or forecast (defaults to tomorrow; add keyword "today" to see the today's forecast - eg "forecast today").
-    Link to known issues and list of things I would like to add: https://ahomeconnected.com/index.php/workin-on-it/`;//})
-        //.then(function() {
-         // console.log(launchMsg+dsMsg+epiMsg);
+    Link to known issues and list of things I would like to add: https://ahomeconnected.com/index.php/workin-on-it/`})
+        .then(function() {
+          console.log(launchMsg+epiMsg);
           notification = service.notifications
             .create({
               toBinding: bindings,
-              body: epiMsg
+              body: launchMsg+epiMsg
             });
-      //  });
+        });
       
