dc233f5609542131d9f59d365645c17676bfbaf2
rlane85 <ryanlane1985@gmail.com>
2019-06-14 21:50:07 -0400

    commit

--- test.js
+++ test.js
@@ -14,37 +14,20 @@ const bindings = numbers.map(number => {
   return JSON.stringify({ binding_type: 'sms', address: number });
 });
 
-request(config.DS_OPTIONS, (err, response, dsData) => {})
-  .then(function(dsData) {
-    dsMsg = `
-Forecast for ${dateFormat(dsData.daily.data[1].time * 1000, "ddd m/d")}:
-${dsData.daily.data[1].summary}
-Sunrise: ${dateFormat(dsData.daily.data[1].sunriseTime * 1000, "h:MM")}
-Sunset: ${dateFormat(dsData.daily.data[1].sunsetTime * 1000, "h:MM")}
-Moonphase: ${dsData.daily.data[1].moonPhase * 100}%
-Feels Like: Low ${dsData.daily.data[1].apparentTemperatureLow}° - High ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
-Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
-Powered by darksky.net
-`});
-request(config.LAUNCH_OPTIONS, (err, response, data) => {})
+request(config.HERE_OPTIONS, (err, response, data) => {})
 .then(function(data) {
-    launchMsg = `
-    Next launch at Cape Canaveral, FL: ${dateFormat(data.results[0].net, "ddd m/d 'at' h:MM t")}
-    Rocket: ${data.results[0].rocket.configuration.name}
-    Launch Agency: ${data.results[0].rocket.configuration.launch_service_provider}
-    Mission: ${data.results[0].mission.name}
-    Status: ${data.results[0].status.name}
-    `})
+msg = `
+Current moon phase: ${data.astronomy.astronomy[0].moonPhase*100}%
+${data.astronomy.astronomy[0].moonPhaseDesc}
+Moon Rise: ${data.astronomy.astronomy[0].moonrise}
+Moon Set: ${data.astronomy.astronomy[0].moonset}
+`})    
 .then(function() {
-    epiMsg = `
-    For more info please reply to this number with a keyword: current, summary, launch, or forecast (defaults to tomorrow; add keyword "today" to see the today's forecast - eg "forecast today").
-    Link to known issues and list of things I would like to add: https://ahomeconnected.com/index.php/workin-on-it/`})
-        .then(function() {
-          console.log(launchMsg+epiMsg+dsMsg);
+          console.log(msg);
           notification = service.notifications
             .create({
               toBinding: bindings,
-              body: launchMsg+epiMsg+dsMsg
+              body: msg
             });
         });
       
