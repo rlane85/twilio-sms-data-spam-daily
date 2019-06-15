93ecf78da97f231fca7d6e29c3731d21135ee202
rlane85 <ryanlane1985@gmail.com>
2019-06-14 20:48:45 -0400

    commit

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-// Grab the two libraries we use for getting data and sending messages
 const request = require('request-promise');
 const config = require('./config');
 const twilio = require('twilio')(
@@ -7,33 +6,45 @@ const twilio = require('twilio')(
 var dateFormat = require('dateformat');
 
 var numbers = new Array();
-numbers = ['+13214033188', '+13213683060'];
+numbers = ['+13214033188'];
 
 const service = twilio.notify.services(process.env.TWILIO_NOTIFY_SERVICE_SID);
 
 const bindings = numbers.map(number => {
   return JSON.stringify({ binding_type: 'sms', address: number });
 });
-request(config.DS_OPTIONS, (err, response, dsData) => {})
+
+/*(config.DS_OPTIONS, (err, response, dsData) => {})
   .then(function(dsData) {
-  dsMsg= `
-Forecast for ${dateFormat(dsData.daily.data[0].time * 1000, "ddd m/d")}:
-${dsData.daily.data[0].summary}
-Sunrise: ${dateFormat(dsData.daily.data[0].sunriseTime * 1000, "h:MM")}
-Sunset: ${dateFormat(dsData.daily.data[0].sunsetTime * 1000, "h:MM")}
-Moonphase: ${dsData.daily.data[0].moonPhase * 100}%
-Feels Like: ${dsData.daily.data[0].apparentTemperatureLow}° - ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[0].apparentTemperatureHighTime * 1000, "h:MM")})
-Rain Chance: ${dsData.daily.data[0].precipProbability * 100}%
+    dsMsg = `
+Forecast for ${dateFormat(dsData.daily.data[1].time * 1000, "ddd m/d")}:
+${dsData.daily.data[1].summary}
+Sunrise: ${dateFormat(dsData.daily.data[1].sunriseTime * 1000, "h:MM")}
+Sunset: ${dateFormat(dsData.daily.data[1].sunsetTime * 1000, "h:MM")}
+Moonphase: ${dsData.daily.data[1].moonPhase * 100}%
+Feels Like: Low ${dsData.daily.data[1].apparentTemperatureLow}° - High ${dsData.daily.data[0].apparentTemperatureHigh}° (at ${dateFormat(dsData.daily.data[1].apparentTemperatureHighTime * 1000, "h:MM")})
+Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
 Powered by darksky.net
 `});
 request(config.LAUNCH_OPTIONS, (err, response, data) => {})
 .then(function(data) {
-  launchMsg = `
-Next launch at Cape Canaveral, FL: ${dateFormat(data.results[0].net, "ddd m/d 'at' h:MM t")}
-Rocket: ${data.results[0].rocket.configuration.name}
-Launch Agency: ${data.results[0].rocket.configuration.launch_service_provider}
-Mission: ${data.results[0].mission.name}
-Status: ${data.results[0].status.name}`})
-.then(function() {
-  console.log(launchMsg);
-  });
+    launchMsg = `
+    Next launch at Cape Canaveral, FL: ${dateFormat(data.results[0].net, "ddd m/d 'at' h:MM t")}
+    Rocket: ${data.results[0].rocket.configuration.name}
+    Launch Agency: ${data.results[0].rocket.configuration.launch_service_provider}
+    Mission: ${data.results[0].mission.name}
+    Status: ${data.results[0].status.name}
+    `})*/
+//.then(function() {
+    epiMsg = `
+    For more info please reply to this number with a keyword: current, summary, launch, or forecast (defaults to tomorrow; add keyword "today" to see the today's forecast - eg "forecast today").
+    Link to known issues and list of things I would like to add: https://ahomeconnected.com/index.php/workin-on-it/`;//})
+        //.then(function() {
+         // console.log(launchMsg+dsMsg+epiMsg);
+          notification = service.notifications
+            .create({
+              toBinding: bindings,
+              body: epiMsg
+            });
+      //  });
+      
