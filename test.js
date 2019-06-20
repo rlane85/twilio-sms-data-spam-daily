0ed5c42169bfc097c8b9197428b1070d2f46634d
rlane85 <ryanlane1985@gmail.com>
2019-06-20 17:22:37 -0400

    commit

--- test.js
+++ test.js
@@ -13,32 +13,30 @@ const request = require('request-promise');
       const bindings = numbers.map(number => {
         return JSON.stringify({ binding_type: 'sms', address: number });
       });
-function builder(){    
-request(config.DS_OPTIONS, (err, response, dsData) => {
+
+request(config.DS_OPTIONS, (err, response, dsData) => {}).then(function(data){
   dsMsg = 
 `Forecast for ${dateFormat(dsData.daily.data[1].time * 1000, "ddd m/d")}: ${dsData.daily.data[1].summary}
 Rain Chance: ${dsData.daily.data[1].precipProbability * 100}%
 `});
-request(config.LAUNCH_OPTIONS, (err, response, data) => {
+request(config.LAUNCH_OPTIONS, (err, response, data) => {}).then(function(data){
   launchMsg =
 `Next launch at Cape Canaveral, FL: ${dateFormat(data.results[0].net, "ddd m/d 'at' h:MM t")}
 `});
-request(config.WU_OPTIONS, (err, response, data) => {
+request(config.WU_OPTIONS, (err, response, data) => {}).then(function(data){
    currentMsg=
 `Current from WU Station ${data.observations[0].stationID}
 Temp: ${data.observations[0].imperial.temp}
 `});
-request(config.WUSUMMARY_OPTIONS, (err, response, data) => {
+request(config.WUSUMMARY_OPTIONS, (err, response, data) => {}).then(function(data){
     summaryMsg=
 `Today's summary from WU Station ${data.summaries[6].stationID}
 Wind gust high: ${data.summaries[6].imperial.windgustHigh} mph
 `});
-request(config.HERE_OPTIONS, (err, response, data) => {
+request(config.HERE_OPTIONS, (err, response, data) => {}).then(function (data){
     moonMsg =
-`Current moon phase: ${data.astronomy.astronomy[0].moonPhase*100}% ${config.moonEmoji(data.astronomy.astronomy[0].moonPhaseDesc)}${data.astronomy.astronomy[0].moonPhaseDesc}`});
-return;
-}
-builder()
+`Current moon phase: ${data.astronomy.astronomy[0].moonPhase*100}% ${config.moonEmoji(data.astronomy.astronomy[0].moonPhaseDesc)}${data.astronomy.astronomy[0].moonPhaseDesc}`;
+})
 .then(function() {
   msg=launchMsg+dsMsg+currentMsg+summaryMsg+moonMsg;
   console.log(msg);
